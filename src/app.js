3709704205fd344fc65021f763dbec571a041c42
samin99kc <saminkc99@gmail.com>
2024-02-15 11:40:27 +0545

    utili comment

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 import express from "express";
-
+import cros from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CROS_ORGIN,
+    Credential: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+// use for the data send from url
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 export { app };
